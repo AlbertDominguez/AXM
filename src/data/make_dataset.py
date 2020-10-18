c18192f3c488ac26bffec719f89523f82244420c
Albert Dominguez <albertadm99@gmail.com>
2020-10-18 18:51:11 +0200

    added code for modelling

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,42 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import numpy as np
+import pandas as pd
+import sqlite3
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+# from ...features.build_features import pipeline
+
+def pipeline(df):
+    logger = logging.getLogger(__name__)
+    logger.info('running preprocessing pipeline...')
+    df_ret = df.copy()
+    df_ret['committerDate'] = pd.to_datetime(df_ret.committerDate)
+    df_ret['committerTimezone'] = df_ret.committerTimezone.astype('int')
+    df_ret['committerDateLocal'] = df_ret.apply(lambda x: x.committerDate + np.timedelta64(x.committerTimezone,'s'), axis=1)
+    week_days = df_ret.committerDateLocal.dt.dayofweek
+    df_ret = pd.concat([df_ret, pd.get_dummies(week_days, prefix='dayOfWeek')], axis=1)
+    df_ret['committerHourOfDay'] = df_ret.committerDateLocal.apply(lambda x: x.hour)
+    df_ret.drop(columns=['commitHash', 'committerDate', 'committerDateLocal', 'committerTimezone'], inplace=True)
+    df_ret.dropna(axis=0, how='any', inplace=True)
+    return df_ret
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
+#@click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(output_filepath, input_filepath='../../data/raw/technicalDebtDataset.db'):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    conn = sqlite3.connect(input_filepath) # connect to DB
+    df = pd.read_sql('SELECT * FROM dataset', conn)
+    df_ret = pipeline(df)
+    df_ret.to_csv(output_filepath, index=False)
+    return 0
 
 
 if __name__ == '__main__':
---
+++ src/models/train_model.py
@@ -0,0 +1,37 @@
+import joblib
+import numpy as np
+import pandas as pd
+import sys
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+def read_and_split():
+    df_full = pd.read_csv('../../data/processed/processed_data.csv')
+    pos = df_full[df_full.faultInducingBool == 1].copy().reset_index(drop=True)
+    neg = df_full[df_full.faultInducingBool == 0].sample(n=1000, random_state=42)
+    neg.dropna(inplace=True)
+    df_red = pd.concat([neg, pos], axis=0)
+    X = df_red.drop(columns='faultInducingBool').to_numpy()
+    y = df_red.faultInducingBool.to_numpy()
+    trX, valX, trY, valY = train_test_split(X, y, test_size=0.25, random_state=42)
+    return trX, valX, trY, valY, df_red.drop(columns='faultInducingBool').columns
+
+def launch_training():
+    trX, valX, trY, valY, colnames = read_and_split()
+    clf = RandomForestClassifier(n_estimators=500, max_depth=7, random_state=42)
+    clf.fit(trX, trY)
+    print('Training confusion matrices:\n', confusion_matrix(trY, clf.predict(trX)))
+    print('Validation confusion matrices:\n', confusion_matrix(valY, clf.predict(valX)))
+    print('Importances:')
+    for i in range(len(colnames)):
+        print(colnames[i], clf.feature_importances_[i])
+    print('Saving model...')
+    joblib.dump(clf, '../../models/random_forest.joblib')
+    return 0
+
+def main():
+    return launch_training()
+
+if __name__ == '__main__':
+    sys.exit(main())
